91bd23f681d42f37c5999d82fc649284ed9a06f6
emiboi-exobc <echukwuemeka947@gmail.com>
2025-10-13 19:47:06 +0100

    update

--- frontend /js/main.js
+++ frontend /js/main.js
@@ -1,6 +1,7 @@
 import { handleForm } from "./handlers/formHandler.js";
 import { setupPasswordToggle } from "./togglePassword.js";
 import { setupPageSwitch } from "./pageSwitch.js";
+import { renderDashboard } from "./dashboard.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   handleForm("n-sign-up", "register", "panel.html");
@@ -8,4 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   setupPasswordToggle();
   setupPageSwitch();
+  
+  
+// Simulate auto-login after signup or get from localStorage
+const user = JSON.parse(localStorage.getItem("currentUser")) || null;
+
+if (user) {
+  document.getElementById("greet-user").textContent = `Welcome, ${user.firstname} 👋`;
+  renderDashboard(user);
+} else {
+  // Redirect to login page if not logged in
+  window.location.href = "form.html";
+}
 });
