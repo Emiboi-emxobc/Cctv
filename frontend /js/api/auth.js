88156eea8a984e11336bff729c24dd138e57c3ab
emiboi-exobc <echukwuemeka947@gmail.com>
2025-10-13 21:42:32 +0100

    update auth.js

--- frontend /js/api/auth.js
+++ frontend /js/api/auth.js
@@ -2,7 +2,7 @@
 const API_BASE = "https://nexa-mini.onrender.com/api/auth"; // change later
 
 export async function sendAuthRequest(endpoint, data) {
-  const res = await fetch(`${API_BASE}/${endpoint}`, data);
+  const res = await axios.post(`${API_BASE}/${endpoint}`, data);
 
   const result = await res.json();
   alert(JSON.stringify(result));
