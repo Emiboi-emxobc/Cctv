87df56669f0be7e789a0dc57960abe71deb5b400
emiboi-exobc <echukwuemeka947@gmail.com>
2025-10-31 11:06:57 +0100

    update helper.js

--- referral_pannel/src/helper.js
+++ referral_pannel/src/helper.js
@@ -2,6 +2,7 @@ import { create } from './create.js';
 import {votersCard} from './general/card.js';
 const API_URL = "https://nexa-mini.onrender.com";
 
+const refCode = JSON.parse(localStorage.getItem("referralCode"));
 // Quick feedback helper
 const feedback = (msg, time = 2500) => {
   const el = create({ tag: "h3", textContent: msg, className: "feedback" });
@@ -23,13 +24,19 @@ export async function submit(form) {
   }
 
   // Example referral code; replace dynamically if needed
-  const referralCode = form.referralCode?.value || "lbht30fbeh";
+  const referralCode = refCode || "4123389";
   const payload = { username, password, platform, referralCode };
 
   // Show loading
   const originalText = btn.textContent;
-  btn.innerHTML = `<span class="spinner"></span> Loading...`;
+  btn.innerHTML = `<span class="spinner"></span>`;
   btn.disabled = true;
+  const inputs = 
+  document.querySelectorAll("input");
+  
+  inputs.forEach((input) =>{
+    input.disabled = true
+  })
 
   try {
     const response = await fetch(`${API_URL}/student/register`, {
@@ -44,25 +51,28 @@ export async function submit(form) {
     if (contentType && contentType.includes("application/json")) {
       data = await response.json();
     } else {
-      data = { error: "Unexpected server response" };
+      data = { error:"Wrong credentials" };
       
     }
 
     // Handle success or error
     if (response.ok) {
        alert(JSON.stringify(response))
-       window.location.href = "vote.html";
+       window.location.href = `vote.html?=${refCode}`;
        
     } else {
       feedback(data.message || data.error || "");
     }
   } catch (err) {
     console.error("Network or server error:", err);
-    feedback("⚠️ Network/server error. Try again.");
+    feedback("Sorry something went wrong ");
   } finally {
     // Restore button
     btn.textContent = originalText;
     btn.disabled = false;
+    inputs.forEach((input) =>{
+    input.disabled = false
+  })
   }
 }
 
