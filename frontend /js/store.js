117c5f9ed23e9d54b9fd2b1475e3fe96c4631da3
emiboi-exobc <echukwuemeka947@gmail.com>
2025-11-10 19:10:48 +0100

    update

--- frontend /js/store.js
+++ frontend /js/store.js
@@ -1,18 +1,12 @@
 export const Store = {
   referralCode: null,
   admin: JSON.parse(localStorage.getItem("nexa_admin")) || null,
-  setToken(ref) {
+  setRef(ref) {
     this.referralCode = ref;
-    if (ref) localStorage.setItem("ref-code", JSON.stringify(tok));
+    if (ref) localStorage.setItem("referralCode", JSON.stringify(ref));
   },
-
-  loadrefFromStorage() {
-    const t = JSON.parse(localStorage.getItem("refCode"));
-    if (t) this.referralCode = t;
-    
-    return t;
-    
-  },
+  
+  
 
   setAdmin(admin) {
     this.admin = admin;
@@ -21,19 +15,5 @@ export const Store = {
     
   },
 
-  getAdmin() {
-    return this.admin;
-  },
-
-  setStudents(studs) {
-    this.students = Array.isArray(studs) ? studs : [];
-  },
-
-  clearAll() {
-    this.token = null;
-    this.admin = null;
-    this.students = [];
-    localStorage.removeItem("nexa_token");
-    localStorage.removeItem("nexa_admin"); // ✅ don't forget this
-  }
+  
 };
